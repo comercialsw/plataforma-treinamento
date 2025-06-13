3c03f1bbe09c087ac1789fb9108db2f284a120e7
comercialsw <comercial@smartwaybr.com.br>
2025-06-13 10:07:47 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,18 @@
+```javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCTJ1qxAmxW8tksXBi8hFLchSnuvSAYQfY",
+  authDomain: "smartway-treinamentos.firebaseapp.com",
+  projectId: "smartway-treinamentos",
+  storageBucket: "smartway-treinamentos.firebasestorage.app",
+  messagingSenderId: "389500401009",
+  appId: "1:389500401009:web:22182df458fc133cc072d0"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+```
+
+---
